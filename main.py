c1881550cd272ed938b01202cee2e77758e2d1c7
Rajtilak Pal <rajtilakls2510@gmail.com>
2023-05-08 12:00:57 +0530

    Added frontend apis

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import random
 import numpy as np
-import cv2
+import cv2, io, base64
 import time
 from threading import Lock
 import pandas as pd
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import torch
+from PIL import Image
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -14,7 +14,13 @@ cors = CORS(app)
 module1_data = pd.DataFrame({"time": [], "motion": [], "distance": []})
 module2_data = pd.DataFrame({"time": [], "motion": [], "distance": []})
 cam_data = pd.DataFrame({"time": [], "xmin": [], "ymin": [], "xmax": [], "ymax": [], "confidence": [], "class": []})
-severities = (0,0)
+severities = [0,0]
+
+cam_feed = None
+mod1_feed = {"motion": 0, "distance": 0.0, "history": []}
+mod2_feed = {"motion": 0, "distance": 0.0, "history": []}
+mod1_override, mod2_override = False, False
+
 
 module_lock = Lock()
 cam_lock = Lock()
@@ -28,38 +34,77 @@ def alarm_detector():
 
     return 1, 1
 
-@app.route("/", methods=["GET"])
-def sdf():
-    return "HelloWorld"
 
+@app.route("/live", methods=["GET"])
+def live():
+    img = Image.fromarray(cam_feed.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_base64 = base64.b64encode(rawBytes.read())
+    return jsonify({"cam": {"img": img_base64} if cam_feed else None, "modules": [mod1_feed, mod2_feed]})
+
+
+@app.route("/override", methods=["POST"])
+def override():
+    global mod1_override, mod2_override
+    module = int(request.form["module"])
+    override = bool(request.form["override"])
+    if module == 1:
+        mod1_override = override
+    else:
+        mod2_override = override
+    return "Done"
+
+
+@app.route("/control", methods=["POST"])
+def control():
+    global severities
+    module = int(request.form["module"])
+    control = bool(request.form["control"])
+    if module == 1:
+        severities[0] = control
+    else:
+        severities[1] = control
+    return "Done"
 
 @app.route("/module", methods=["POST"])
 def module():
-    global module1_data, module2_data, severities
+    global module1_data, module2_data, severities, mod1_feed, mod2_feed
 
     module = int(request.form["module"])
-    motion = request.form["motion"]
-    distance = request.form["distance"]
+    motion = int(request.form["motion"])
+    distance = float(request.form["distance"])
 
     module_lock.acquire()
     if module == 1:
         module1_data = pd.concat(
             [module1_data, pd.DataFrame({"time": [time.time()], "motion": [motion], "distance": [distance]})])
         module1_data = module1_data[module1_data.time >= (time.time() - 100)]
+        mod1_feed["motion"] = motion
+        mod1_feed["distance"] = distance
     else:
         module2_data = pd.concat(
             [module2_data, pd.DataFrame({"time": [time.time()], "motion": [motion], "distance": [distance]})])
         module2_data = module2_data[module2_data.time >= (time.time() - 100)]
+        mod2_feed["motion"] = motion
+        mod2_feed["distance"] = distance
     module1_data.to_csv("mod1.csv", index=False)  # TODO: Comment in production
     module2_data.to_csv("mod2.csv", index=False)  # TODO: Comment in production
-    severities = alarm_detector()
+    new_severities = alarm_detector()
+    if module == 1 and not mod1_override and new_severities[0] > 0 and severities[0] == 0:
+        mod1_feed["history"].append({"time": time.time(), "severity": new_severities[0]})
+        severities[0] = new_severities[0]
+    if module == 2 and not mod2_override and new_severities[1] > 0 and severities[1] == 0:
+        mod2_feed["history"].append({"time": time.time(), "severity": new_severities[1]})
+        severities[1] = new_severities[1]
     module_lock.release()
     return jsonify({"severity": severities[0] if module == 1 else severities[1]})
 
 
 @app.route("/cam", methods=["POST"])
 def cam():
-    global cam_data
+    global cam_data, cam_feed
     filestr = request.files['imageFile'].read()
     file_bytes = np.frombuffer(filestr, np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_UNCHANGED)
@@ -77,6 +122,14 @@ def cam():
     cam_data = cam_data[cam_data.time >= (time.time() - 100)]
     cam_data.to_csv("cam.csv", index=False) # TODO: Comment in production
     cam_lock.release()
+    animals = [0, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+    newpred = []
+    for pred in results.pred[0]:
+        if int(pred[-1]) in animals:
+            newpred.append(pred)
+    results.pred[0] = torch.stack(newpred) if len(newpred) > 0 else torch.Tensor([])
+    results.render()
+    cam_feed = results.ims[0]
     return "Received"
 
 
