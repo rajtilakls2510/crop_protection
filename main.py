44559bde828016b431222acb44fe7c858c1ad423
gopiyush <aggarwalpiyush06@gmail.com>
2023-05-10 15:58:50 +0530

    Created alarm

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ cors = CORS(app)
 module1_data = pd.DataFrame({"time": [], "motion": [], "distance": []})
 module2_data = pd.DataFrame({"time": [], "motion": [], "distance": []})
 cam_data = pd.DataFrame({"time": [], "xmin": [], "ymin": [], "xmax": [], "ymax": [], "confidence": [], "class": []})
-severities = [0,1]
+severities = [0,0]
 
 
 cam_feed = cv2.imread("placeholder.png")
@@ -32,73 +32,79 @@ def alarm_detector():
     global module1_data, module2_data, cam_data
 
     # TODO: Process
-
     # Defining some of my own Hyperparameters, Variables, and Flags
     window_size = 15
     motion_sensitivity = 3
-    domestic_animals = ["horse", "sheep", "cow", "zebra", "donkey", "cat", "dog", "camel"]
-    wild_animals = ["elephant", "bear", "boar", "cheetah", "leopard", "lion"]
+    domestic_animals = ["cat", "dog", "horse", "sheep", "cow"]
+    wild_animals = ["elephant", "bear", "zebra", "giraffe"]
     severity = 0
+    yolo_sensitivity = 0.35
+    try:
+        # Fetch rows from cam data within window size
+        sorted_df = cam_data.sort_values('time', ascending=False)
+        t0 = sorted_df.iloc[0]['time']
+        cam_window = sorted_df[(sorted_df['time'] >= t0) & (sorted_df['time'] <= t0+window_size)]
 
-    # Sort dataframes on time and fetch last rows of window size
-    module1_window = module1_data.sort_values('time').tail(window_size)
-    module2_window = module2_data.sort_values('time').tail(window_size)
-    cam_window = cam_data.sort_values('time').tail(window_size)
+        # Sort dataframes on time and fetch last rows of window size
+        module1_window = module1_data.sort_values('time').tail(window_size)
+        module2_window = module2_data.sort_values('time').tail(window_size)
 
-    # Module 1
-    module1_motion_flag = (module1_window['motion'] == 1).sum() >= motion_sensitivity
-    module1_distance = module1_window['distance'].min()
+        # Module 1
+        module1_motion_flag = (module1_window['motion'] == 1).sum() >= motion_sensitivity
+        module1_distance = module1_window['distance'].min()
 
-    # Module 2
-    module2_motion_flag = (module2_window['motion'] == 1).sum() >= motion_sensitivity
-    module2_distance = module2_window['distance'].min()
+        # Module 2
+        module2_motion_flag = (module2_window['motion'] == 1).sum() >= motion_sensitivity
+        module2_distance = module2_window['distance'].min()
 
-    # Camera
-    domestic_rows = cam_window[cam_window['class'].isin(domestic_animals)]
-    wild_rows = cam_window[cam_window['class'].isin(wild_animals)]
-    if not domestic_rows.empty:
-        confidence_domestic_animals = domestic_rows['confidence'].mean()
-        if (confidence_domestic_animals >= .75):
-            count_domestic_animals = domestic_rows['class'].count()
+        # Camera
+        domestic_rows = cam_window[cam_window['class'].isin(domestic_animals)]
+        wild_rows = cam_window[cam_window['class'].isin(wild_animals)]
+        if not domestic_rows.empty:
+            confidence_domestic_animals = domestic_rows['confidence'].mean()
+            if (confidence_domestic_animals >= yolo_sensitivity):
+                count_domestic_animals = domestic_rows['class'].count()
+            else:
+                count_domestic_animals = 0
         else:
             count_domestic_animals = 0
-    else:
-        count_domestic_animals = 0
-    if not wild_rows.empty:
-        confidence_wild_animals = wild_rows['confidence'].mean()
-        if (confidence_wild_animals >= .75):
-            count_wild_animals = wild_rows['class'].count()
+        if not wild_rows.empty:
+            confidence_wild_animals = wild_rows['confidence'].mean()
+            if (confidence_wild_animals >= yolo_sensitivity):
+                count_wild_animals = wild_rows['class'].count()
+            else:
+                count_wild_animals = 0
         else:
             count_wild_animals = 0
-    else:
-        count_wild_animals = 0
 
-    # Logic
-    if count_domestic_animals == 0 and count_wild_animals == 0:
-        severity = 0  # Level 0: No animals detected
-    elif count_domestic_animals < 5 and count_wild_animals == 0:
-        severity = 1  # Level 1: Low alert, no wild animals
-    elif 5 <= count_domestic_animals < 20 or 1 < count_wild_animals < 5:
-        severity = 2  # Level 2: Moderate alert, few wild or some domestic animals
-    elif 20 <= count_domestic_animals < 50 or 5 <= count_wild_animals < 10:
-        severity = 3  # Level 3: High alert, huge animals
-    elif count_domestic_animals >= 50 or count_wild_animals >= 10:
-        severity = 4  # Level 4: Extreme alert, CM ko jaga do
+        # Logic
+        if count_domestic_animals == 0 and count_wild_animals == 0:
+            severity = 0  # Level 0: No animals detected
+        elif count_domestic_animals < 5 and count_wild_animals == 0:
+            severity = 1  # Level 1: Low alert, no wild animals
+        elif 5 <= count_domestic_animals < 20 or 1 < count_wild_animals < 5:
+            severity = 2  # Level 2: Moderate alert, few wild or some domestic animals
+        elif 20 <= count_domestic_animals < 50 or 5 <= count_wild_animals < 10:
+            severity = 3  # Level 3: High alert, huge animals
+        elif count_domestic_animals >= 50 or count_wild_animals >= 10:
+            severity = 4  # Level 4: Extreme alert, CM ko jaga do
 
-    # Return logic
-    if (module1_distance > 250):
-        module1_motion_flag = False
-    if (module2_distance > 250):
-        module2_motion_flag = False
-    if module1_motion_flag and module2_motion_flag:
-        return severity, severity
-    elif module1_motion_flag:
-        return severity, 0
-    elif module2_motion_flag:
-        return 0, severity
-    else:
-        return 0, 0
-
+        # Return logic
+        if (module1_distance > 250):
+            module1_motion_flag = False
+        if (module2_distance > 250):
+            module2_motion_flag = False
+        if module1_motion_flag and module2_motion_flag:
+            return severity, severity
+        elif module1_motion_flag:
+            return severity, 0
+        elif module2_motion_flag:
+            return 0, severity
+        else:
+            return 0,0
+    except Exception as e:
+        print("The error is: ", e)
+        return severities
 
 @app.route("/live", methods=["GET"])
 def live():
